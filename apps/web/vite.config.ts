86c906ed0e6fe32cbd322b8f448a29e799e17bef
Gunther Brunner <gunta85@gmail.com>
2025-10-29 15:56:50 +0900

    vercel

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+			"@slidont/backend": path.resolve(__dirname, "../../packages/backend"),
 		},
 	},
 });
